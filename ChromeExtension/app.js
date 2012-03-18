be6060344e6464db905cf8bad5a2e68341483c95
Yangyang Zhao <yangyang.zhao.thu@gmail.com>
2012-03-18 14:35:41 +0800

    add logout

--- ChromeExtension/app.js
+++ ChromeExtension/app.js
@@ -19,26 +19,10 @@ Ext
 
 			  var background = chrome.extension.getBackgroundPage();
 			  var oauth = background.oauth;
-
-			  if (oauth.hasToken())
-			  {
-			  }else{
+			  var loginHandler = function(btn, evt) {
 				background.authorize();
-			  }
+			  };
 
-			  function checkNewMondai() {
-				var today = new Date().format('Y-m-d');
-				var doneToday = localStorage[today];
-				if( doneToday == undefined ) {
-				  chrome.browserAction.setBadgeText({text: '1'}); 
-				}else{
-				  chrome.browserAction.setBadgeText({text: ''}); 
-				}
-			  }
-
-			  function getMondai(date) {
-				return localStorage[date];
-			  }
 
 			  function updateMondaiUI() {
 				Ext
@@ -99,7 +83,7 @@ Ext
 				}
 			  }
 			  var tapHandler = function(btn, evt) {
-				checkNewMondai();
+				background.checkNewMondai();
 				inner.setActiveItem(formback, 'slide');
 				var url = Ext.getCmp('choice1').getGroupValue();
 				var result = localStorage[url];
@@ -146,6 +130,35 @@ Ext
 			  var formback;
 			  var mondai;
 			  var kaisetsu;
+			  var welcome;
+
+
+			  welcome = {
+				standardSubmit : false,
+				width: '400px',
+				height: '600px',
+				items: [ {
+				  xtype : 'panel',
+				  html : '<br><center>今日的日本语检定Chrome插件是由poweruser开发#今日的日本语检定#的Chrome插件，所用数据均来自<a href="http://www.jiji.com" media="handheld" rel=external>時事ドットコム</a>，并<b>不用作商业用途</b>。欢迎指出bug，提出更好修改建议！</center><center>联系作者:<a href="mailto:yangyang.zhao.thu@gmail.com">yangyang.zhao.thu@gmail.com</a><br></center><br>',
+				},
+				{
+				  xtype: 'spacer'
+				},
+				{
+				  xtype : 'button',
+				  ui : 'round',
+				  text : '微博授权',
+				  handler : loginHandler
+				}],
+				dockedItems : [ 
+				{
+				  xtype : 'toolbar',
+				  dock : 'top',
+				  title : '今日的日本语检定',
+				  items : [ ]
+				} ]
+			  };
+
 			  mondai = {
 				standardSubmit : false,
 				width: '400px',
@@ -215,11 +228,16 @@ Ext
 								},{
 								  xtype : 'spacer'
 								}, {
-								  iconMask : true,
+								  iconMask: true,
 								  ui : 'plain',
-								  iconCls : 'favorites',
+								  iconCls : 'bookmarks',
 								  handler : function() {
-									background.updateWeibo('我正在使用#今日的日本语检定#，你也来用用吧！');
+									if (calendarView.hidden == true) {
+									  calendarView.show();
+									} else {
+									  calendarView.hide();
+									}
+
 								  }
 								} ]
 							  } ]
@@ -281,7 +299,15 @@ Ext
 				items : [
 				{
 				  xtype : 'panel',
-				  html : '<center>今日的日本语检定Chrome插件是由poweruser开发#今日的日本语检定#的Chrome插件，所用数据均来自<a href="http://www.jiji.com" media="handheld" rel=external>時事ドットコム</a>，并<b>不用作商业用途</b>。欢迎指出bug，提出更好修改建议！</center><center>联系作者:<a href="mailto:yangyang.zhao.thu@gmail.com">yangyang.zhao.thu@gmail.com</a><br></center>',
+				  html : '<br><center>今日的日本语检定Chrome插件是由poweruser开发#今日的日本语检定#的Chrome插件，所用数据均来自<a href="http://www.jiji.com" media="handheld" rel=external>時事ドットコム</a>，并<b>不用作商业用途</b>。欢迎指出bug，提出更好修改建议！</center><center>联系作者:<a href="mailto:yangyang.zhao.thu@gmail.com">yangyang.zhao.thu@gmail.com</a><br></center><br>',
+				},{
+				  xtype : 'button',
+				  ui : 'round',
+				  text : '推荐到新浪微博',
+				  handler : function() {
+					background.updateWeibo('我正在使用#今日的日本语检定Chrome插件#，感觉不错哦，你也来试试吧 http://kyounonihonngo.sinaapp.com/');
+				  }
+
 				} ],
 				dockedItems : [ {
 								xtype : 'toolbar',
@@ -292,10 +318,20 @@ Ext
 								  ui : 'back',
 								  text : '后退',
 								  handler : tapHandler2
-								} ]
+								}, {
+								  xtype: 'spacer'
+								},{ 
+								  text: '登出',
+								  ui : 'action',
+								  handler : function() {
+									oauth.clearTokens();
+								  }
+								}]
 							  } ]
 			  };
 
+
+			  var formwelcome = new Ext.form.FormPanel(welcome);
 
 			  form = new Ext.form.FormPanel(mondai);
 
@@ -303,16 +339,26 @@ Ext
 
 			  formabout = new Ext.form.FormPanel(about);
 
-
-			  var inner = new Ext.Panel({
-				layout : 'card',
+			  if(oauth.hasToken()) {
+				var inner = new Ext.Panel({
+				  layout : 'card',
 				  fullscreen : true,
 				  width: '400px',
 				  height: '600px',
 				  items : [ form, formback, formabout ]
-			  });
-			  inner.show();
-			  updateMondai(selectedDate);
-			  checkNewMondai();
+				});
+				inner.show();
+				updateMondai(selectedDate);
+				background.checkNewMondai();
+			  }else {
+				var inner = new Ext.Panel({
+				  layout : 'card',
+				  fullscreen : true,
+				  width: '400px',
+				  height: '600px',
+				  items : [ formwelcome]
+				});
+				inner.show();
+			  }
 			}
 });
